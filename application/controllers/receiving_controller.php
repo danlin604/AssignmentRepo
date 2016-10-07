045f1937841a45ec9dfd6b5e5d2f899ca2c9491e
Manveer Bhangu <manu.bhangu5530@gmail.com>
2016-10-06 17:05:27 -0700

    created supplies model and recieving page
    Signed-off-by: Manveer Bhangu <manu.bhangu5530@gmail.com>

---
+++ application/controllers/receiving_controller.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * The receiving page shows a complete list of supplies used by the resturant.
+ * Needs to have a way to specifying how many receiving units are being accepted
+ * into stock. (need to get the receiving units from the supplies model and
+ * update the currAvail in the stock model). Could do a number field beside 
+ * and a prompt for the update?
+ *
+*/
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class receiving_controller extends Application
+{
+
+	public function index()
+	{	
+		// Option #2, need to display the contents on the view of the receiving view, don't need revieving cell 
+		
+		//view we want to show
+		$this->data['pagebody'] = 'recieving_view';
+
+		// build the list of authors, to pass on to our view
+		$source = $this->supplies_model->all();
+		$items = array ();
+		foreach ($source as $record)
+		{
+			$items[] = array ('id' => $record['id'], 'Desc' => $record['Desc'], 'receiving_unit' => $record['receiving_unit']);
+		}
+		$this->data['items'] = $items;
+
+		$this->render();
+		//$this->data = array_merge($this->data, $items);
+
+	}
+}
