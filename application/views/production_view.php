5828c103a64127b2ab3a9edc30c353b29e82dd53
Daniel Lin <danlin604@gmail.com>
2016-10-06 18:54:25 -0700

    Fixed the template content menu overlap, and added a dashboard for menu
    Signed-off-by: Daniel Lin <danlin604@gmail.com>

--- application/views/production_view.php
+++ application/views/production_view.php
@@ -1,17 +1,3 @@
-<br><br><br><br><br>
 <h1>McDonalds Legendary Recipes</h1>
 
 {production_table}
-
-
-<?php
-/*
-/*
-QUICK Production_View page REQUIREMENTS...
-
-- show all recipes
-- For the selected one, show the ingredients that go into it flagging any that are not on hand. 
-- Log any items made, without updating inventory. ???????
-
-*/ 
-?>
--- application/views/sales_view.php
+++ application/views/sales_view.php
@@ -1,18 +1,3 @@
-<br><br><br><br><br>
 <h1>Try our delicious menu!</h1>
 
 {sales_table}
-
-
-
-
-<?php
-/*
-SALES VIEW REQUIREMENTS... 
-
- -Show a menu of purchaseable items with description + price for each. 
- -The goal of this page is to build an order with multiple items, and to log the transaction that would result if the sale proceeded for real.
-
-
-*/
-?>
