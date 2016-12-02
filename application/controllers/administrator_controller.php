4061e1afaaa81175fec73da28f95d986f6df5b9c
Daniel Lin <danlin604@gmail.com>
2016-12-02 13:05:48 -0800

    Incomplete form
    Signed-off-by: Daniel Lin <danlin604@gmail.com>

--- application/controllers/administrator_controller.php
+++ application/controllers/administrator_controller.php
@@ -14,58 +14,80 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class administrator_controller extends Application
 {
+    
+    function __construct() {
+        parent::__construct();
+        $this->load->helper('formfields');
+        $this->error_messages = array();
+    }
 
-	public function index()
-	{
-                $userrole = $this->session->userdata('userrole');
-                if ($userrole != 'admin') {
-                    $message = 'You are not authorized to access this page. Go away';
-                    $this->data['content'] = $message;
-                }
-                //$message = 'Get ready to fix stuff.';
-                //$this->data['pagebody' ] ='mtce';
-                //$this->data['items'] = $this->menu->all();
-                //$this->render();
-		
-                //view we want to show
-		$this->data['pagebody'] = 'admin_view';
+    public function index()
+    {
+        $userrole = $this->session->userdata('userrole');
+        if ($userrole != 'admin') {
+            $message = 'You are not authorized to access this page. Go away';
+            $this->data['content'] = $message;
+        }
 
-		// gets the contents from the 3 models
-		//$supplies_source = $this->supplies_model->all();
-		//$recipes_source = $this->recipes_model->all();
-		//$stock_source = $this->stock_model->all();
-		
-		// arrays to hold the contents of the models
-		//$supplies = array();
-		//$recipes = array();
-		//$stock = array();
-		
-		// go through the sources and store the data in there respective arrays
-		$result_supplies = ' ';
-		foreach ($this->supplies->all() as $record)
-		{
-			$result_supplies .= $this->parser->parse('supplies_admin', $record, true);
-		}
-		$this->data['supplies'] = $result_supplies;
+        //view we want to show
+        $this->data['pagebody'] = 'admin_view';
 
+        // go through the sources and store the data in there respective arrays
+        $result_supplies = ' ';
+        foreach ($this->supplies->all() as $record)
+        {
+                $result_supplies .= $this->parser->parse('supplies_admin', $record, true);
+        }
+        $this->data['supplies'] = $result_supplies;
 
-		$result_recipes = ' ';
-		foreach ($this->recipes->all() as $record)
-		{
-			$result_recipes .= $this->parser->parse('recipes_admin', $record, true);
-		}
-		$this->data['recipes'] = $result_recipes;
+        $result_recipes = ' ';
+        foreach ($this->recipes->all() as $record)
+        {
+                $result_recipes .= $this->parser->parse('recipes_admin', $record, true);
+        }
+        $this->data['recipes'] = $result_recipes;
 
+        $result_stock = ' ';
+        foreach ($this->stock->all() as $record)
+        {
+                $result_stock .= $this->parser->parse('stock-admin', $record, true); 
+        }
+        $this->data['stock'] = $result_stock;
+        $this->render();
+    }
+        
+    function edit($id=null) {
+        
+        // try the session first
+        $key = $this->session->userdata('key');
+        $record = $this->session->userdata('record');
 
-		$result_stock = ' ';
-		foreach ($this->stock->all() as $record)
-		{
-			$result_stock .= $this->parser->parse('stock-admin', $record, true); 
-		}
-		$this->data['stock'] = $result_stock;
+        // if not there, get them from the database
+        if (empty($record) && ($id !== null)) {
+            $record = $this->stock->get($id);
+            $key = $id;
+            $this->session->set_userdata('key',$id);
+            $this->session->set_userdata('record',$record);
+        }
 
-		$this->render();
-		//$this->data = array_merge($this->data, $items);
+        $this->data['action'] = (empty($key)) ? 'Adding' : 'Editing';
 
-	}
+        // build the form fields
+        //$this->data['fid'] = makeTextField('Menu code', 'id', $record->id);
+        //$this->data['fname'] = makeTextField('Item name', 'name', $record->name);
+        //$this->data['fdescription'] = makeTextArea('Description', 'description', $record->description);
+        //$this->data['fprice'] = makeTextField('Price, each', 'price', $record->price);
+        //$this->data['fpicture'] = makeTextField('Item image', 'picture', $record->picture);
+
+        //$cats = $this->categories->all(); // get an array of category objects
+        //foreach ($cats as $code => $category) // make it into an associative array
+        //    $codes[$category->id] = $category->name;
+        //$this->data['fcategory'] = makeCombobox('Category', 'category', $record->category, $codes);          
+        //$this->data['zsubmit'] = makeSubmitButton('Save', 'Submit changes');
+
+        // show the editing form
+        $this->data['pagebody'] = "stock-edit";
+        //$this->show_any_errors();
+        $this->render();
+    }
 }
--- application/views/stock-admin.php
+++ application/views/stock-admin.php
@@ -4,7 +4,7 @@
       <td class="mdl-data-table__cell--non-numeric"><p class="desc">{description}</p></td>
       <td>{price}</td>
       <td>{currAvail}</td>
-      <td><button type="edit" onClick="alert('add functionality here')">Edit</button></td>
+      <td><a href="/admin/edit/{id}">Edit {id}</a></td>
       <td><button type="delete" onClick="alert('add functionality here')">Delete</button></td>
     </tr>
   </tbody>
